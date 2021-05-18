40b6dfe034a49bcce059a8c110f830e6be161784
Chris Morris <chris.morris13@nhs.net>
2021-05-18 12:50:04 +0100

    Schema file to update configuration tables

---
+++ database/schema/V1.8__UpdateConfigurationTables.sql
@@ -0,0 +1,13 @@
+/*
+    Schema V1.8 - update Configuration tables
+*/
+
+INSERT INTO [Configuration].[SplunkInstance]
+           ([SplunkInstance]
+           ,[SplunkInstanceGroup])
+     VALUES
+           ('other',
+           'other')
+GO
+
+
