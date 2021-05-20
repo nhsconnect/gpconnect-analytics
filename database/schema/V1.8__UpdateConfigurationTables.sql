f437d06b02377045673056b5d82c77cffd6b8746
Chris Morris <chris.morris13@nhs.net>
2021-05-20 12:06:52 +0100

    Moved splunk client configuration changes to new file.

--- database/schema/V1.8__UpdateConfigurationTables.sql
+++ database/schema/V1.8__UpdateConfigurationTables.sql
@@ -9,13 +9,3 @@ INSERT INTO [Configuration].[SplunkInstance]
            ('other',
            'other')
 GO
-
-ALTER TABLE Configuration.SplunkClient ADD ApiToken VARCHAR(1000) NULL;
-
-UPDATE Configuration.SplunkClient SET ApiToken='*** SET APITOKEN VALUE HERE ***';
-
-ALTER TABLE Configuration.SplunkClient ALTER COLUMN ApiToken VARCHAR(1000) NOT NULL;
-
-ALTER TABLE Configuration.SplunkClient ADD CONSTRAINT CK_Configuration_SplunkClient_ApiToken  CHECK (trim(ApiToken ) != '')
-
-
