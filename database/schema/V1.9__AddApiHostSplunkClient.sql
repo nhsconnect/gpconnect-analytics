309562f2b82581dcb3da29b7a9c01619398c1536
Chris Morris <chris.morris13@nhs.net>
2021-05-20 12:11:39 +0100

    Fix to the DB script for ApiHost in  Splunk Client configuration

--- database/schema/V1.9__AddApiHostSplunkClient.sql
+++ database/schema/V1.9__AddApiHostSplunkClient.sql
@@ -1,6 +1,13 @@
-ALTER TABLE Configuration.SplunkClient ADD ApiToken VARCHAR(1000) NULL;
-UPDATE Configuration.SplunkClient SET ApiToken='*** SET APITOKEN VALUE HERE ***';
-ALTER TABLE Configuration.SplunkClient ALTER COLUMN ApiToken VARCHAR(1000) NOT NULL;
-ALTER TABLE Configuration.SplunkClient ADD CONSTRAINT CK_Configuration_SplunkClient_ApiToken  CHECK (trim(ApiToken) != '');
+ALTER TABLE Configuration.SplunkClient ADD ApiToken VARCHAR(1000) NULL
+GO
+
+UPDATE Configuration.SplunkClient SET ApiToken='*** SET APITOKEN VALUE HERE ***'
+GO
+
+ALTER TABLE Configuration.SplunkClient ALTER COLUMN ApiToken VARCHAR(1000) NOT NULL
+GO
+
+ALTER TABLE Configuration.SplunkClient ADD CONSTRAINT CK_Configuration_SplunkClient_ApiToken  CHECK (trim(ApiToken) != '')
+GO
 
 
